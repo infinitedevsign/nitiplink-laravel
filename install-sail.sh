e6299780932b78d107a50fb4dfeff74927455a8e
Fariz <nizaralfariziakbar10@gmail.com>
2024-01-06 02:05:43 +0700

    change to php81

--- install-sail.sh
+++ install-sail.sh
@@ -2,5 +2,5 @@ docker run --rm \
     -u "$(id -u):$(id -g)" \
     -v $(pwd):/opt \
     -w /opt \
-    laravelsail/php80-composer:latest \
+    laravelsail/php81-composer:latest \
     composer install --ignore-platform-reqs
