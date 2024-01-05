9aae1dc33c3e9a26b9b44298c1a7a41d5ad87462
Fariz <nizaralfariziakbar10@gmail.com>
2024-01-06 03:24:53 +0700

    add update script

---
+++ update.sh
@@ -0,0 +1,6 @@
+git pull origin main &&
+./vendor/bin/sail up -d &&
+./vendor/bin/sail composer install &&
+./vendor/bin/sail artisan migrate &&
+./vendor/bin/sail bun run build &&
+./vendor/bin/sail down && sail up -d
