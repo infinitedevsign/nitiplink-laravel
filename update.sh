95e63cb4ad2e8901e711206ec5316964a4ab51e5
Fariz <nizaralfariziakbar10@gmail.com>
2024-01-06 03:32:53 +0700

    update

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@ git pull origin main &&
 ./vendor/bin/sail composer install &&
 ./vendor/bin/sail artisan migrate &&
 ./vendor/bin/sail bun run build &&
-./vendor/bin/sail down && sail up -d
+./vendor/bin/sail down &&
+./vendor/bin/sail up -d
